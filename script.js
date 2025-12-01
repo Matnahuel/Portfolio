1511120ae6d3f07f6aa2cdd36da0b42a6f3cb212
Matnahuel <matias.nahuel.carabajal.mc@gmail.com>
2025-12-01 18:39:42 -0300

    click derecho y arajos de herramientas de desarrollador desactivados

--- script.js
+++ script.js
@@ -252,3 +252,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+    }
+});
