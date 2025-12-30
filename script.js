9f99d259a666da252de5b7c1c5bc88a70809f8f3
Matnahuel <matias.nahuel.carabajal.mc@gmail.com>
2025-12-30 14:21:43 -0300

    aplicacion de responsive

--- script.js
+++ script.js
@@ -788,12 +788,4 @@ document.addEventListener("DOMContentLoaded", () => {
             status.textContent = 'Error de red al enviar el mensaje.';
         }
     });
-
-    // Prevent context menu and dev tools
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.key === 'U')) {
-            e.preventDefault();
-        }
-    });
 });
